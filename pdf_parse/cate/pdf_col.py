bc41381825470fd267167cfa18c285285e8eed07
kaixin <1577134779@qq.com>
2023-09-01 17:36:52 +0800

    同一列

---
+++ pdf_parse/cate/pdf_col.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+from PIL.Image import Image
+
+from pdf_parse.base import PdfBase
+
+
+class Col(PdfBase):
+
+    def parse(self, result, **kwargs):
+        img = result.orig_img
+
+        # 存放图片
+        titles = list()
+        images = list()
+
+        for index, box in enumerate(result.boxes.xyxy):
+            # 获取图片坐标
+            x1, y1, x2, y2 = box.numpy().astype(int).tolist()
+
+            # 获取款类型
+            class_str = result.names[int(result.boxes.cls[index])]
+
+            # 根据坐标分割图片
+            img2 = img[y1:y2, x1:x2]
+
+            # 存放标题和图片信息
+            if class_str == 'title':
+
+                # 将BGR类型的图片转换为RGB的类型
+
+                img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+
+                img2 = cv2.imencode(".png", img2)[1]
+                # 识别title
+                class_name = self.ocr.classification(img2.tobytes())
+                titles.append({
+                    "title": class_name,
+                    "pos": (x1, y1, x2, y2)
+
+                })
+            elif class_str == 'imgBord':
+                images.append({
+                    "img": img2,
+                    "pos": (x1, y1, x2, y2)
+                })
+
+        # 根据定位对应上title和图片
+        if len(titles) != len(images): return
+
+        # 识别图片在同一列
+        for title in titles:
+            for image in images:
+                if image['pos'][2] >= title['pos'][0] >= image['pos'][0] or image['pos'][2] >= title['pos'][2] >= image['pos'][0]:
+                    # print(title, image)
+                    # cv2.imshow("title['title']", image['img'])
+                    # cv2.imwrite(str(index)+'.png', image['img'])
+                    # print("================================================================")
+                    # cv2.waitKey(0)
+                    yield title['title'], image['img']
+
+
+if __name__ == '__main__':
+    Col().start(r"C:\Users\Administrator\Downloads\zhongyaotupu")
